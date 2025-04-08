7707253191ee9d4bad103f0d9259528aedc26642
Madhavan1707 <madhvan02@gmail.com>
2025-04-08 23:50:50 +0530

    -

--- app/src/main/java/com/example/task/CastAdapter.java
+++ app/src/main/java/com/example/task/CastAdapter.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import java.util.List;
-
+      
 public class CastAdapter extends RecyclerView.Adapter<CastAdapter.CastViewHolder> {
 
     private List<CastMember> castList;
